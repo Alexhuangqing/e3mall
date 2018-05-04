9b19acdc67650d8155d5d8ff3ca38389e847aaa6
Alexhuangqing <Alex_up_up@163.com>
2018-05-04 19:50:55 +0800

    修改.gitignore

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -3,16 +3,67 @@
  */
 package cn.e3mall.service;
 
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 
 /**
- * @author Alex
- * 2018年3月3日 
- * @className  ItemService
- * @Description
+ * @author Alex 2018年4月14日
+ *         <p>
+ * 		desc:商品相关
+ *         </p>
  */
-
-
 public interface ItemService {
-	public TbItem getItemById(Long id );
+
+	/**
+	 * 查询单个商品
+	 * 
+	 * @param id
+	 * @return TbItem
+	 *         <p>
+	 * 		desc:查询单个商品
+	 *         </p>
+	 */
+	public TbItem getItemById(Long id);
+
+	/**
+	 * 查询单个商品的大文本
+	 * 
+	 * @param id
+	 * @return
+	 *         <p>
+	 * 		desc:查询单个商品的大文本
+	 *         </p>
+	 */
+	public TbItemDesc getItemDescById(Long id);
+
+	/**
+	 * 表格分页信息展示
+	 * 
+	 * @param page
+	 *            当前页码
+	 * @param rows
+	 *            每页数量
+	 * @return 分页展示信息
+	 *         <p>
+	 * 		desc:分页显示所有商品
+	 *         </p>
+	 */
+	public EasyUIDataGridResult getList(int page, int rows);
+
+	/**
+	 * 添加商品
+	 * 
+	 * @param tbItem
+	 *            封装添加的商品信息
+	 * @param desc
+	 *            封装添加商品描述
+	 * @return E3Result 状态结果
+	 *         <p>
+	 * 		desc:添加单个商品
+	 *         </p>
+	 */
+	public E3Result addItem(TbItem tbItem, String desc);
+
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -3,42 +3,199 @@
  */
 package cn.e3mall.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.dubbo.common.utils.StringUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.jedis.JedisClient;
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.common.utils.IDUtils;
+import cn.e3mall.common.utils.JsonUtils;
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.pojo.TbItemExample.Criteria;
 import cn.e3mall.service.ItemService;
 
 /**
- * @author Alex
- * 2018年3月3日 
- * @className 
- * @Description
+ * @author Alex 2018年4月14日
+ *         <p>
+ *         desc:null
+ *         </p>
  */
 @Service
 public class ItemServiceImpl implements ItemService {
+	/**
+	 * Autowired 根据类型注入 Resource 根据id注入（如下 Destination 有两种实现 只能根据id注入）
+	 */
 	@Autowired
-	private  TbItemMapper tbItemMapper;
-	
-	
-	/* (non-Javadoc)
+	private TbItemMapper tbItemMapper;
+
+	@Autowired
+	private TbItemDescMapper tbItemDescMapper;
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Resource
+	private Destination topicDestination;
+
+	@Autowired
+	private JedisClient jedisClient;
+
+	@Value(value = "ITEM_INFO_PRE")
+	private String ITEM_INFO;
+	@Value(value = "ITEM_EXPIRE_TIME")
+	private Integer ITEM_EXPIRE_TIME;
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see cn.e3mall.service.ItemService#getItemById(long)
 	 */
 	@Override
 	public TbItem getItemById(Long id) {
+		try {
+			// 查找缓存 缓存操作是独立的 异常不应该不影响正常的操作
+			String itemStr = jedisClient.get(ITEM_INFO + ":" + id + ":BASE");
+			if (StringUtils.isNotEmpty(itemStr)) {
+				return JsonUtils.jsonToPojo(itemStr, TbItem.class);
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
 		TbItemExample example = new TbItemExample();
 		Criteria criteria = example.createCriteria();
 		criteria.andIdEqualTo(id);
 		List<TbItem> tbItems = tbItemMapper.selectByExample(example);
-		if(tbItems != null && tbItems.size()>0){
+		if (tbItems != null && tbItems.size() > 0) {
+			try {
+				// 将查找的数据加入缓存
+				jedisClient.set(ITEM_INFO + ":" + id + ":BASE", JsonUtils.objectToJson(tbItems.get(0)));
+				// 设置过期时间
+				jedisClient.expire(ITEM_INFO + ":" + id + ":BASE", ITEM_EXPIRE_TIME);
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+
 			return tbItems.get(0);
 		}
 		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see cn.e3mall.service.ItemService#getList(int, int)
+	 */
+	@Override
+	public EasyUIDataGridResult getList(int page, int rows) {
+		PageHelper.startPage(page, rows);
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		long total = pageInfo.getTotal();
+
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setRows(list);
+		result.setTotal(total);
+
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see cn.e3mall.service.ItemService#addItem(cn.e3mall.pojo.TbItem,
+	 * java.lang.String)
+	 */
+	@Override
+	public E3Result addItem(TbItem tbItem, String desc) {
+		final Long id = IDUtils.genItemId();
+		// 添加记录到商品表
+		tbItem.setId(id);
+		tbItem.setStatus((byte) 1);
+		Date createde = new Date();
+		Date Updated = new Date();
+		tbItem.setCreated(createde);
+		tbItem.setUpdated(Updated);
+		tbItemMapper.insert(tbItem);
+		// 添加记录到商品描述表
+		TbItemDesc tbItemDesc = new TbItemDesc();
+		tbItemDesc.setItemId(id);
+		tbItemDesc.setItemDesc(desc);
+		tbItemDesc.setCreated(createde);
+		tbItemDesc.setUpdated(Updated);
+		tbItemDescMapper.insert(tbItemDesc);
+		// 将id 添加到ActiveMQ 用于append同步索引库
+		jmsTemplate.send(topicDestination, new MessageCreator() {
+			// 为保证consumer在接收时，insert事务已经完成提交
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+
+				return session.createTextMessage(id + "");
+			}
+
+		});
+
+		return E3Result.ok();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see cn.e3mall.service.ItemService#getItemDescById(java.lang.Long)
+	 */
+	@Override
+	public TbItemDesc getItemDescById(Long id) {
+
+		try {
+			// 查找缓存 缓存操作是独立的 异常不应该不影响正常的操作
+			String itemStr = jedisClient.get(ITEM_INFO + ":" + id + ":BASE");
+			if (StringUtils.isNotEmpty(itemStr)) {
+				return JsonUtils.jsonToPojo(itemStr, TbItemDesc.class);
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		TbItemDesc itemDesc = tbItemDescMapper.selectByPrimaryKey(id);
+		if (itemDesc != null) {
+			try {
+				// 将查找的数据加入缓存
+				jedisClient.set(ITEM_INFO + ":" + id + ":DESC", JsonUtils.objectToJson(itemDesc));
+				// 设置过期时间
+				jedisClient.expire(ITEM_INFO + ":" + id + ":DESC", ITEM_EXPIRE_TIME);
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+
+		}
+		return itemDesc;
 	}
 
 }
